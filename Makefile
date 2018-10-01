986e96815dff2d82ac8aa35223af5a94f50608f7
Chris Atkin <atkinchris@gmail.com>
2018-10-01 22:26:17 +0100

    Use bash build script

--- Makefile
+++
@@ -1,6 +0,0 @@
-build:
-	cargo build --release
-	rm -rf releases
-	mkdir -p releases
-	tar -czf releases/kubesecrets-0.1.0.tar.gz --directory=target/release kubesecrets
-	shasum -a 256 releases/kubesecrets-0.1.0.tar.gz > releases/kubesecrets-0.1.0.tar.gz.shasum
---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+########################################################
+# Script to build and package a release                #
+# usage: ./release.sh                                  #
+########################################################
+
+
+# Fail on the first error, rather than continuing
+set -e
+
+VERSION=$(cat Cargo.toml | sed -n -e 's/^.*version = "//p'  | sed -e 's/^"//' -e 's/"$//')
+FILENAME="releases/kubesecrets-${VERSION}.tar.gz"
+
+cargo build --release
+rm -rf releases
+mkdir -p releases
+tar -czf $FILENAME --directory=target/release kubesecrets
+shasum -a 256 $FILENAME > $FILENAME.shasum
+
+git tag "v${VERSION}"
